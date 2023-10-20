7b9d442d1565decd1b091716061a265491557a8d
Prasad19122002 <147840443+Prasad19122002@users.noreply.github.com>
2023-10-20 16:08:22 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,17 +1,36 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+    $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST["email"], FILTER_VALIDATE_EMAIL);
+    $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
 
-    $to = "prasadsmart2002@email.com";
-    $subject = "Contact Form Submission from $name";
-    $headers = "From: $email";
+    if ($name && $email && $message) {
+        $to = "prasadsmart@gmail.com"; // Replace with your email address
+        $subject = "Contact Form Submission from $name";
+        $headers = "From: $email\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
-    mail($to, $subject, $message, $headers);
+        $emailBody = "
+            <html>
+            <body>
+                <p>Name: $name</p>
+                <p>Email: $email</p>
+                <p>Message:</p>
+                <p>$message</p>
+            </body>
+            </html>
+        ";
 
-    echo "Thank you for your message. I will get back to you soon.";
+        if (mail($to, $subject, $emailBody, $headers)) {
+            echo "Thank you for your message. I will get back to you soon.";
+        } else {
+            echo "Oops! Something went wrong while sending your message. Please try again later.";
+        }
+    } else {
+        echo "Invalid data submitted. Please check your inputs and try again.";
+    }
 } else {
-    echo "Something went wrong. Please try again later.";
+    echo "Invalid request method. Please try again later.";
 }
 ?>
