5aa09a0a8c356041b9f94f51af889aef32bded0c
Prasad19122002 <147840443+Prasad19122002@users.noreply.github.com>
2023-10-20 14:35:32 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,13 +1,17 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    // You can add more validation and processing here (e.g., sending an email).
-    // For this example, we'll just return a response message.
+    $to = "prasadsmart2002@email.com";
+    $subject = "Contact Form Submission from $name";
+    $headers = "From: $email";
 
-    $response = "Thank you, $name! Your message has been received.";
-    echo $response;
+    mail($to, $subject, $message, $headers);
+
+    echo "Thank you for your message. I will get back to you soon.";
+} else {
+    echo "Something went wrong. Please try again later.";
 }
 ?>
