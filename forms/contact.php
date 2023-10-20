f0210a3dc7b1f29045439176b1ec16c516d8a1b4
Prasad19122002 <147840443+Prasad19122002@users.noreply.github.com>
2023-10-20 16:23:09 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,25 +1,13 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
-    $email = filter_var($_POST["email"], FILTER_VALIDATE_EMAIL);
-    $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
 
-    if ($name && $email && $message) {
-        $to = "prasadsmart@gmail.com"; // Replace with your email address
-        $subject = "Contact Form Submission from $name";
-        $headers = "From: $email\r\n";
-        $headers .= "MIME-Version: 1.0\r\n";
-        $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+    // You can add further processing, validation, and email sending here.
 
-        if (mail($to, $subject, $emailBody, $headers)) {
-            echo "Thank you for your message. I will get back to you soon.";
-        } else {
-            echo "Oops! Something went wrong while sending your message. Please try again later.";
-        }
-    } else {
-        echo "Invalid data submitted. Please check your inputs and try again.";
-    }
+    echo "Thank you for your message, $name. I will get back to you soon.";
 } else {
-    echo "Invalid request method. Please try again later.";
+    echo "Something went wrong. Please try again later.";
 }
 ?>
