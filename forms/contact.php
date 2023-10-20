77b45b7505984bc652d1fe7a93baf86f6aa98ad7
Prasad19122002 <147840443+Prasad19122002@users.noreply.github.com>
2023-10-20 14:13:57 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,21 +1,13 @@
 <?php
-  $receiving_email_address = 'prasadsmart2002@gmail.com';
-  if( file_exists($php_email_form = 'assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+    // You can add more validation and processing here (e.g., sending an email).
+    // For this example, we'll just return a response message.
 
-  echo $contact->send();
+    $response = "Thank you, $name! Your message has been received.";
+    echo $response;
+}
 ?>
