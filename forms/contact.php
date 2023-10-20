172aa78748e8d78d5f27939b8750e224492f2eb0
Prasad19122002 <147840443+Prasad19122002@users.noreply.github.com>
2023-10-20 16:59:30 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,34 +1,34 @@
 <?php
 
-if(isset($_POST)){
-	$formok = true;
-
-	//form data
-
-	$name = htmlspecialchars($_POST['name']);
-	$email = htmlspecialchars($_POST['email']);
-	$message = htmlspecialchars($_POST['message']);
-
-	//validation
-	if(empty($name) || empty($email) || empty($message)){
-		$formok = false;
-	}
-
-	if($formok){
-		$headers = "From: info@nandccontractors.com" . "\r\n";
-		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-		$emailbody = "<p>You have recieved a new message from the enquiries form on your website.</p>
-                  <p><strong>Name: </strong> {$name} </p>
-                  <p><strong>Email Address: </strong> {$email} </p>
-                  <p><strong>Message: </strong> {$message} </p> ";
-
-    mail("prasadsmart2002@gmail.com","New Enquiry",$emailbody,$headers);
-
-	}
-
-        //redirect back to form
-        header('location: ' . $_SERVER['HTTP_REFERER']);
-
-}
-
- ?>
+    $receiving_email_address = 'myemailadress';
+    
+      if( file_exists($php_email_form = '/assets/vendor/php-email-form/php-email-form.php' )) {
+        include( $php_email_form );
+      } else {
+        die( 'Coming Soon!');
+      }
+    
+      $contact = new PHP_Email_Form;
+      $contact->ajax = true;
+      
+      $contact->to = $receiving_email_address;
+      $contact->from_name = $_POST['name'];
+      $contact->from_email = $_POST['email'];
+      $contact->subject = $_POST['subject'];
+    
+      // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+      /*
+      $contact->smtp = array(
+        'host' => 'example.com',
+        'username' => 'example',
+        'password' => 'pass',
+        'port' => '587'
+      );
+      */
+    
+      $contact->add_message( $_POST['name'], 'From');
+      $contact->add_message( $_POST['email'], 'Email');
+      $contact->add_message( $_POST['message'], 'Message', 10);
+    
+      echo $contact->send();
+    ?>
