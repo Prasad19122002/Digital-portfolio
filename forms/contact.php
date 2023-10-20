cf2df63029d2a3638450fe20fe54a4faabadc1de
Prasad19122002 <147840443+Prasad19122002@users.noreply.github.com>
2023-10-20 18:11:25 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,6 +1,6 @@
 <?php
 
-    $receiving_email_address = 'myemailadress';
+    $receiving_email_address = 'prasadc1912@gmail.com';
     
       if( file_exists($php_email_form = '/assets/vendor/php-email-form/php-email-form.php' )) {
         include( $php_email_form );
